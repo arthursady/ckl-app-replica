93a9c1af282016a5e64a870612038a07123b258f
Arthur Sady Cordeiro Rossetti <arthursady@gmail.com>
2017-01-14 16:58:52 -0200

    Implement login button activation and deactivation

--- CKLApp-Replica/app/src/main/java/com/challenge/cklapp_replica/LoginActivity.kt
+++ CKLApp-Replica/app/src/main/java/com/challenge/cklapp_replica/LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.challenge.cklapp_replica
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.ViewManager
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.view.*
+import kotlinx.android.synthetic.main.main_login.*
 
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var loginFragment : LoginFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_login)
-        val loginFragment = LoginFragment().newInstance()
+        loginFragment = LoginFragment().newInstance()
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.login_view,loginFragment,"login_fragment")
--- CKLApp-Replica/app/src/main/java/com/challenge/cklapp_replica/LoginFragment.kt
+++ CKLApp-Replica/app/src/main/java/com/challenge/cklapp_replica/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.challenge.cklapp_replica
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.view.*
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+
+
 
 /**
  * Created by Arthur on 12/01/2017.
  */
-class LoginFragment() : Fragment() {
+class LoginFragment : Fragment() {
 
     fun newInstance() : LoginFragment{
         return this
@@ -20,22 +25,58 @@ class LoginFragment() : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        var view:View = inflater!!.inflate(R.layout.activity_login,container,false)
-        view.login_cklLogo.setImageResource(context.resources.getIdentifier("ckl_logo","drawable",
-                context.packageName))
-        view.login_logo.setImageResource(context.resources.getIdentifier("login_image","drawable",
-                context.packageName))
-        view.password_logo.setImageResource(context.resources.getIdentifier("password_logo",
-                "drawable",context.packageName))
+        val loginView:View = inflater!!.inflate(R.layout.activity_login,container,false)
 
-        view.sign_in_button.setOnClickListener {
-            Toast.makeText(context, "You Tried to Sign In",
+        loginView.login_cklLogo.setImageResource(context.resources
+                .getIdentifier("ckl_logo","drawable", context.packageName))
+
+        loginView.login_logo.setImageResource(context.resources
+                .getIdentifier("login_image","drawable", context.packageName))
+
+        loginView.password_logo.setImageResource(context.resources
+                .getIdentifier("password_logo","drawable",context.packageName))
+
+        loginView.edit_login.setOnFocusChangeListener { view, b ->
+            checkSignInStatus(loginView)
+        }
+
+        checkSignInStatus(loginView)
+
+        loginView.edit_password.setOnEditorActionListener { textView, i, keyEvent ->
+            when(i) {
+                EditorInfo.IME_ACTION_DONE -> {
+
+                    loginView.clearFocus()
+                    val inputManager = context
+                            .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+                    inputManager.hideSoftInputFromWindow(loginView.windowToken,
+                            InputMethodManager.HIDE_NOT_ALWAYS)
+                }
+            }
+
+            true
+        }
+
+        loginView.sign_in_button.setOnClickListener {
+
+            Toast.makeText(context, loginView.edit_login.text,
                     Toast.LENGTH_SHORT).show()
         }
 
-        return view
+        return loginView
     }
 
-
-
+    fun checkSignInStatus(loginView:View){
+        if(!(loginView.edit_login.text.isEmpty())&& !(loginView.edit_password.text.isEmpty())) {
+            loginView.sign_in_button.background = resources
+                    .getDrawable(R.drawable.round_corner_button)
+            loginView.sign_in_button.isEnabled = true
+        }
+        else{
+            loginView.sign_in_button.background = resources
+                    .getDrawable(R.drawable.round_corner_button_midblue)
+            loginView.sign_in_button.isEnabled = false
+        }
+    }
 }
